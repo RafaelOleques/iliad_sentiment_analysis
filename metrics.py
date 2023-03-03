218deaa7b288fb9496f1c42636531046b6fa85d4
Rafael Fernandes Borges <rafaeelfernandes@hotmail.com>
2023-03-03 16:51:11 -0300

    refactor metrics

--- metrics.py
+++ metrics.py
@@ -1,61 +1,104 @@
-def metricas(confusion_matrix):
+def print_metrics(acc, err, prec_macro, prec_ind, rev_macro, rev_ind, spec_macro, spec_ind):
+    print("Acurácia: ", acc)
+    print("Taxa de erro: ", err, '\n')
+
+    print("Precisão(macro): ", prec_ind)
+    for i in range(len(prec_ind)):
+        print("  Precisão(" + ohe.categories_[0][i] + "): ", prec_ind[i])
+
+    print('\n', "Recall(macro): ", rev_macro)
+    for i in range(len(rev_ind)):
+        print("  Recall(" + ohe.categories_[0][i] + "): ", rev_ind[i])
+
+    print('\n', "Especificidade(macro): ", spec_macro)
+    for i in range(len(spec_ind)):
+        print("  Especificidade(" + ohe.categories_[0][i] + "): ", spec_ind[i])
+
+def list_mean(lst):
+    mean = 0
+    for i in range(len(lst)):
+        mean += lst[i]
+    
+    mean /= len(lst)
+    
+    return mean
+    
+def accuracy(confusion_matrix):
     total = 0
-    verdadeiros = 0
+    true_values = 0
+    for i in range(len(confusion_matrix)):
+        for j in range(len(confusion_matrix[i])):
+            total += confusion_matrix[i][j]
+            if i == j:
+                true_values += confusion_matrix[i][j]
+
+    accuracy = true_values / total
+    
+    return accuracy
+
+def error_rate(confusion_matrix):
+    err = 1 - accuracy(confusion_matrix)
+    
+    return err
+
+def individual_precision_list(confusion_matrix):
+    cols = [0] * len(confusion_matrix)
+    for i in range(len(confusion_matrix)):
+        for j in range(len(confusion_matrix[i])):
+            cols[j] += confusion_matrix[i][j]
+    
+    individual_precision = [0] * len(confusion_matrix)
+    for i in range(len(confusion_matrix)):
+        individual_precision[i] = confusion_matrix[i][i] / cols[i]
+        
+    return individual_precision
+
+def macro_precision(confusion_matrix):
+    precision = list_mean(individual_precision_list(confusion_matrix))
+    
+    return precision
+
+def individual_recall_list(confusion_matrix):
     lines = [0] * len(confusion_matrix)
+    for i in range(len(confusion_matrix)):
+        for j in range(len(confusion_matrix[i])):
+            lines[i] += confusion_matrix[i][j]
+
+    individual_recall = [0] * len(confusion_matrix)
+    for i in range(len(confusion_matrix)):
+        individual_recall[i] = confusion_matrix[i][i] / lines[i]    
+    
+    return individual_recall
+
+def macro_recall(confusion_matrix):
+    recall = list_mean(individual_recall_list(confusion_matrix))
+    
+    return recall
+    
+def individual_specificity_list(confusion_matrix):
+    total = 0
     cols = [0] * len(confusion_matrix)
     vn = [0] * len(confusion_matrix)
     for i in range(len(confusion_matrix)):
         for j in range(len(confusion_matrix[i])):
             total += confusion_matrix[i][j]
-            lines[i] += confusion_matrix[i][j]
             cols[j] += confusion_matrix[i][j]
             vn[i] -= confusion_matrix[i][j]
             vn[j] -= confusion_matrix[i][j]
             if i == j:
                 vn[i] += confusion_matrix[i][j]
-                verdadeiros += confusion_matrix[i][j]
-    
+                
     for i in range(len(vn)):
         vn[i] += total
 
-    acuracia = verdadeiros / total
-    taxa_de_erro = 1 - acuracia
-    precisao_individual = [0] * len(confusion_matrix)
-    recall_individual = [0] * len(confusion_matrix)
-    especificidade_individual = [0] * len(confusion_matrix)
-    
+    individual_specificity = [0] * len(confusion_matrix)
     for i in range(len(confusion_matrix)):
-        recall_individual[i] = confusion_matrix[i][i] / lines[i]
-        precisao_individual[i] = confusion_matrix[i][i] / cols[i]
-        especificidade_individual[i] = vn[i] / (cols[i] - confusion_matrix[i][i] + vn[i])
+        individual_specificity[i] = vn[i] / (cols[i] - confusion_matrix[i][i] + vn[i])
     
-    precisao_macro = 0
-    recall_macro = 0
-    especificidade_macro = 0
-    for i in range(len(confusion_matrix)):
-        precisao_macro += precisao_individual[i]
-        recall_macro += recall_individual[i]
-        especificidade_macro += especificidade_individual[i]
-        
-    precisao_macro /= len(confusion_matrix)
-    recall_macro /= len(confusion_matrix)
-    especificidade_macro /= len(confusion_matrix)
+    return individual_specificity
+
+def macro_specificity(confusion_matrix):
+    specificity = list_mean(individual_specificity_list(confusion_matrix))
     
-    return  acuracia, taxa_de_erro, precisao_macro, precisao_individual, recall_macro, recall_individual, especificidade_macro, especificidade_individual
-
-def print_metricas(acc, err, prec_macro, prec_ind, rev_macro, rev_ind, spec_macro, spec_ind):
-    print("Acurácia: ", acc)
-    print("Taxa de erro: ", err, '\n')
-
-    print("Precisão(macro): ", prec_macro)
-    for i in range(len(cm_test)):
-        print("  Precisão(" + ohe.categories_[0][i] + "): ", prec_ind[i])
-
-    print('\n', "Recall(macro): ", rev_macro)
-    for i in range(len(cm_test)):
-        print("  Recall(" + ohe.categories_[0][i] + "): ", rev_ind[i])
-
-    print('\n', "Especificidade(macro): ", spec_macro)
-    for i in range(len(cm_test)):
-        print("  Especificidade(" + ohe.categories_[0][i] + "): ", spec_ind[i])
+    return specificity
 
