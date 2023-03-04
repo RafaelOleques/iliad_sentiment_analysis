d2a79aa849cf43e38072d54f6ab7d2a5edfceb72
Rafael Fernandes Borges <rafaeelfernandes@hotmail.com>
2023-03-04 03:00:52 -0300

    bugfix metrics formula

--- metrics.py
+++ metrics.py
@@ -1,19 +1,3 @@
-def print_metrics(acc, err, prec_macro, prec_ind, rev_macro, rev_ind, spec_macro, spec_ind):
-    print("Acurácia: ", acc)
-    print("Taxa de erro: ", err, '\n')
-
-    print("Precisão(macro): ", prec_ind)
-    for i in range(len(prec_ind)):
-        print("  Precisão(" + ohe.categories_[0][i] + "): ", prec_ind[i])
-
-    print('\n', "Recall(macro): ", rev_macro)
-    for i in range(len(rev_ind)):
-        print("  Recall(" + ohe.categories_[0][i] + "): ", rev_ind[i])
-
-    print('\n', "Especificidade(macro): ", spec_macro)
-    for i in range(len(spec_ind)):
-        print("  Especificidade(" + ohe.categories_[0][i] + "): ", spec_ind[i])
-
 def list_mean(lst):
     mean = 0
     for i in range(len(lst)):
@@ -22,8 +6,8 @@ def list_mean(lst):
     mean /= len(lst)
     
     return mean
-    
-def accuracy_score(confusion_matrix):
+
+def accuracy(confusion_matrix):
     total = 0
     true_values = 0
     for i in range(len(confusion_matrix)):
@@ -34,30 +18,30 @@ def accuracy_score(confusion_matrix):
 
     accuracy = true_values / total
     
-    return accuracy * 100
+    return accuracy
 
 def error_rate(confusion_matrix):
-    err = 1 - accuracy_score(confusion_matrix)
+    err = 1 - accuracy(confusion_matrix)
     
-    return err * 100
+    return err
 
 def individual_precision_list(confusion_matrix):
     cols = [0] * len(confusion_matrix)
     for i in range(len(confusion_matrix)):
-        for j in range(len(confusion_matrix[i])):
+        for j in range(len(confusion_matrix)):
             cols[j] += confusion_matrix[i][j]
     
     individual_precision = [0] * len(confusion_matrix)
     for i in range(len(confusion_matrix)):
-        if not cols[i] == 0:
+        if cols[i] > 0:
             individual_precision[i] = confusion_matrix[i][i] / cols[i]
         
-    return individual_precision * 100
+    return individual_precision
 
 def macro_precision(confusion_matrix):
     precision = list_mean(individual_precision_list(confusion_matrix))
     
-    return precision * 100
+    return precision
 
 def individual_recall_list(confusion_matrix):
     lines = [0] * len(confusion_matrix)
@@ -67,16 +51,15 @@ def individual_recall_list(confusion_matrix):
 
     individual_recall = [0] * len(confusion_matrix)
     for i in range(len(confusion_matrix)):
-        if not lines[i] == 0:
-            individual_recall[i] = confusion_matrix[i][i] / lines[i]    
+        individual_recall[i] = confusion_matrix[i][i] / lines[i]    
     
-    return individual_recall * 100
+    return individual_recall
 
 def macro_recall(confusion_matrix):
     recall = list_mean(individual_recall_list(confusion_matrix))
     
-    return recall * 100
-    
+    return recall
+
 def individual_specificity_list(confusion_matrix):
     total = 0
     cols = [0] * len(confusion_matrix)
@@ -97,16 +80,34 @@ def individual_specificity_list(confusion_matrix):
     for i in range(len(confusion_matrix)):
         individual_specificity[i] = vn[i] / (cols[i] - confusion_matrix[i][i] + vn[i])
     
-    return individual_specificity * 100
+    return individual_specificity
 
 def macro_specificity(confusion_matrix):
     specificity = list_mean(individual_specificity_list(confusion_matrix))
     
-    return specificity * 100
+    return specificity
 
 def f1_score(confusion_matrix):
     prec = macro_precision(confusion_matrix)
     recall = macro_recall(confusion_matrix)
     score = (2 * prec * recall) / (prec + recall)
 
-    return score 
+    return score
+
+def print_metrics(acc, err, prec_macro, prec_ind, rev_macro, rev_ind, spec_macro, spec_ind, f_score):
+    print("Acurácia: ", acc)
+    print("Taxa de erro: ", err, '\n')
+
+    print("Precisão(macro): ", prec_macro)
+    for i in range(len(prec_ind)):
+        print("  Precisão(" + ohe.categories_[0][i] + "): ", prec_ind[i])
+
+    print('\n', "Recall(macro): ", rev_macro)
+    for i in range(len(rev_ind)):
+        print("  Recall(" + ohe.categories_[0][i] + "): ", rev_ind[i])
+
+    print('\n', "Especificidade(macro): ", spec_macro)
+    for i in range(len(spec_ind)):
+        print("  Especificidade(" + ohe.categories_[0][i] + "): ", spec_ind[i])
+    
+    print('\n', "F1-score: ", f_score)
